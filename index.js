94cce8116ae6bf0fa623c5317027e1b85d28fb98
hegartim <hegartim@mail.uc.edu>
2022-07-21 21:32:09 -0400

    Added game logic for player 1

--- index.js
+++ index.js
@@ -20,6 +20,9 @@ const game = new Phaser.Game(config);
 let ball;
 let player1;
 let player2;
+let isGameStarted = false;
+let cursors;
+const paddleSpeed = 350;
 
 function preload(){
     this.load.image("ball", "assets/images/ball.png");
@@ -33,20 +36,52 @@ function create(){
         this.physics.world.bounds.height / 2,
         "ball"
     )
+    ball.setCollideWorldBounds(true);
+    ball.setBounce(1, 1);
 
     player1=this.physics.add.sprite(
         this.physics.world.bounds.width - (ball.body.width / 2 + 1),
         this.physics.world.bounds.height / 2,
         "paddle"
     )
+    player1.setImmovable(true);
+    player1.setCollideWorldBounds(true);
 
     player2=this.physics.add.sprite(
         ball.body.width / 2 + 1,
         this.physics.world.bounds.height / 2,
         "paddle"
     )
+    player2.setImmovable(true);
+    player2.setCollideWorldBounds(true);
+
+    cursors = this.input.keyboard.createCursorKeys();
+
+    this.physics.add.collider(ball, player1);
+    this.physics.add.collider(ball, player2);
 }
 
 function update(){
+    if(!isGameStarted){
+        const initialVelocityX = 100;
+        const initialVelocityY = 100;
+        ball.setVelocityX(initialVelocityX);
+        ball.setVelocityY(initialVelocityY);
+        isGameStarted = true;
+    }
 
+    player1.body.setVelocityY(0)
+    if (cursors.up.isDown){
+        player1.body.setVelocityY(-paddleSpeed);
+    }
+    if (cursors.down.isDown){
+        player1.setVelocity(paddleSpeed);
+    }
+    if(ball.body.velocity.y > paddleSpeed){
+        ball.body.setVelocityY(paddleSpeed);
+    }
+    if(ball.body.velocity.y < -paddleSpeed){
+        ball.body.setVelocityY(-paddleSpeed);
+    }
+    
 }
